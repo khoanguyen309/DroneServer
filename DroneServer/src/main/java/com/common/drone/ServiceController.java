74eb696a284aa42efcde911060ef62ed5186a711
khoa.nguyen309@gmail.com <khoa.nguyen309@gmail.com>
2014-04-17 12:20:44 -0700

    Init. rest_service

--- DroneServer/src/main/java/com/common/drone/ServiceController.java
+++ DroneServer/src/main/java/com/common/drone/ServiceController.java
@@ -1,5 +1,9 @@
 package com.common.drone;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +17,11 @@ public class ServiceController
 	@RequestMapping(value = "/time", method = RequestMethod.GET)
 	public String GetServerTime()
 	{
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+		
+		String formattedDate = dateFormat.format(date);
+		
 		return "test_service";
 	}
 }
